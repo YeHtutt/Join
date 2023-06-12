02b2f8b9fd8e80a746ad83e7a1da5634c41c90a5
YeHtutAung <yehaung.nov@gmail.com>
2023-06-13 00:14:57 +0200

    improved for responsive summary page

--- js/summary.js
+++ js/summary.js
@@ -152,3 +152,13 @@ function checkingConditions() {
     }
 
 }
+
+window.addEventListener('scroll', myFunction);
+
+function myFunction() {
+  if (scrollY == 0) {
+    document.getElementById('kanban').classList.remove('display-unset');
+  } else{
+    document.getElementById('kanban').classList.add('display-unset');
+  }
+}
